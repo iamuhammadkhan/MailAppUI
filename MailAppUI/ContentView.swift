7aa637810b8ba618518ad28356a7024a5d75009c
iamuhammadkhan <19494818+iamuhammadkhan@users.noreply.github.com>
2021-08-27 01:08:43 +0500

    Initial Commit

---
+++ MailAppUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MailAppUI
+//
+//  Created by Muhammad Khan on 8/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MailAppUI/MailAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailAppUIApp.swift
+//  MailAppUI
+//
+//  Created by Muhammad Khan on 8/27/21.
+//
+
+import SwiftUI
+
+@main
+struct MailAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
